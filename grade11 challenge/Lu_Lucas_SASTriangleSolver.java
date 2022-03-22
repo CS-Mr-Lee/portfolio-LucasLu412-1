ed6d88a4297a74a8a1143e838ddb5fbeaed49e11
LucasLu412 <72579916+LucasLu412@users.noreply.github.com>
2022-03-22 00:22:57 -0400

    Update Lu_Lucas_SASTriangleSolver.java

--- grade11 challenge/Lu_Lucas_SASTriangleSolver.java
+++ grade11 challenge/Lu_Lucas_SASTriangleSolver.java
@@ -1,3 +1,5 @@
+package classwork;
+
 /**
 * Name: Lucas Lu
 * Date: Feb.15
@@ -11,15 +13,15 @@ public class Lu_Lucas_SASTriangleSolver{
     	 Scanner in = new Scanner(System.in);
     	 
     	 System.out.println("please enter angle in radian between two sides");
-    	 double angleRadian = in.nextDouble();
+    	 double angleGivenInRadian = in.nextDouble();
     	 
     	 System.out.println("Please enter the first side of the triangle");
-    	 double side1 = in.nextDouble();
+    	 double firstSideGiven = in.nextDouble();
     	 
     	 System.out.println("please enter the second side of the triangle");
-    	 double side2 = in.nextDouble();
+    	 double secondSideGiven = in.nextDouble();
     	 
-    	 double smallestAngle = triangleSolver(angleRadian,side1,side2);
+    	 double smallestAngle = triangleSolver(angleGivenInRadian,firstSideGiven,secondSideGiven);
     	 
     	 System.out.printf("The smallest angle in triangle is: %.2f", smallestAngle);
     	 
@@ -27,36 +29,35 @@ public class Lu_Lucas_SASTriangleSolver{
      }
      
      /**
-     * convert radian angle to the degree and calculate all anlges in the triangle, then return smallest one in degree
+     * convert radian angle to the degree and calculate all angles in the triangle, then return smallest one in degree
      * @param angleRadian  double input value of angle in radian
      * @param side1  the first side beside that angle
      * @param side2  the second side beside that angle
      * @return return smallest angle in degree
      */
-     public static double triangleSolver(double angleRadian, double side1, double side2) {
-      //get the degree of angle given in radian
-    	 double triangleDegree = angleRadian;
-    	 double triangleSide1 = side1;
-    	 double triangleSide2 = side2;
+     public static double triangleSolver(double angleGivenInRadian, double firstSideGiven, double secondSideGiven) {
+         //get the degree of angle given in radian
+    	 double angleFirstGiven = angleGivenInRadian;
+    	 double firstSideBesideGivenAngle = firstSideGiven;
+    	 double secondSideBesideGivenAngle = secondSideGiven;
     	 
-      //get the third sides and other angles
-    	 double side3 = Math.sqrt(Math.pow(triangleSide1, 2) + Math.pow(triangleSide2, 2) - (2 * triangleSide1 * triangleSide2 * Math.cos(triangleDegree)));
-      double secondDegree = Math.acos((Math.pow(triangleSide2,2) + Math.pow(side3,2) - Math.pow(triangleSide1,2)) / (2 * side3 * triangleSide2));
-      secondDegree = secondDegree * 180 / 3.1415926;
+         //get the third side and other angles
+    	 double ThirdSide = Math.sqrt(Math.pow(firstSideBesideGivenAngle, 2) + Math.pow(secondSideBesideGivenAngle, 2) - (2 * firstSideBesideGivenAngle * secondSideBesideGivenAngle * Math.cos(angleFirstGiven)));
+         double secondAngleInRadian = Math.acos((Math.pow(secondSideBesideGivenAngle,2) + Math.pow(ThirdSide,2) - Math.pow(firstSideBesideGivenAngle,2)) / (2 * ThirdSide * secondSideBesideGivenAngle));
+         double secondAngleInDegree = secondAngleInRadian * 180 / 3.1415926;  //convert second angle to degree
        
-      //convert all angles to the degree
-    	 double thirdDegree = 180 - triangleDegree - secondDegree;
-      triangleDegree = triangleDegree * 180 / 3.1415926;
+         //convert all angles to the degree
+    	 double thirdAngle = 180 - angleFirstGiven - secondAngleInDegree; 
+    	 angleFirstGiven = angleFirstGiven * 180 / 3.1415926;  //convert first angle to degree
     	 
-      //compare those angles and return the smallest one
-    	 if (secondDegree < triangleDegree && secondDegree < thirdDegree) {
-    	   return secondDegree;
-    	 } else if (triangleDegree < secondDegree && triangleDegree < thirdDegree) {
-    	   return triangleDegree;
-    	 } else if (thirdDegree < secondDegree && thirdDegree < triangleDegree) {
-    	   return thirdDegree;
+         //compare those angles and return the smallest one
+    	 if (secondAngleInDegree < angleFirstGiven && secondAngleInDegree < thirdAngle) {
+    	   return secondAngleInDegree;
+    	 } else if (angleFirstGiven < secondAngleInDegree && angleFirstGiven < thirdAngle) {
+    	   return angleFirstGiven;
+    	 } else if (thirdAngle < secondAngleInDegree && thirdAngle < angleFirstGiven) {
+    	   return thirdAngle;
     	 } 
     	 return 0;
      }
 }
-
